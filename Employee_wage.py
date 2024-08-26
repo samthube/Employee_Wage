aa58b5149c29086055e5fed5016eb2bd170b3df0
Sam Thube <samadhanthube444@gmail.com>
2024-08-26 17:10:55 +0530

    [Samadhan] Modify: Changed Instance method to class method

--- Employee_wage.py
+++ Employee_wage.py
@@ -15,15 +15,14 @@ class Employee_Wage:
     MAX_WORKING_DAYS = 20
     MAX_WORKING_HOURS = 100
     
-    def __init__(self):
-        self.daily_wages = []
-        self.total_days = 0
-        self.total_hours = 0
-        self.full_time_count = 0
-        self.part_time_count = 0
-        self.leaves_count = 0
-         
-    @classmethod   
+    daily_wages = []
+    total_days = 0
+    total_hours = 0
+    full_time_count = 0
+    part_time_count = 0
+    leaves_count = 0
+    
+    @classmethod
     def check_attendance(cls):
         """
         Description:
@@ -50,47 +49,46 @@ class Employee_Wage:
             tuple: The wages for a full day and a part-time day.
         """
         return cls.WAGE_PER_HOUR * cls.FULL_DAY_HOUR, (cls.WAGE_PER_HOUR // 2) * cls.FULL_DAY_HOUR 
-        
-    def wage_for_month(self, full_day_wage, part_time_wage):
+
+    @classmethod
+    def wage_for_month(cls):
         """
         Description:
-            This function simulates the employee's attendance and wage computation over a 20-day or 100 hours work period.
-            It tracks the number of full-time days, part-time days, and leave days.
+            This class method simulates the employee's attendance and wage computation over a 20-day or 100 hours work period.
+            It tracks the number of full-time days, part-time days, and leave days using class variables.
         
         Parameter:
-            full_day_wage (int): The wage for a full day of work.
-            part_time_wage (int): The wage for a part-time day of work.
+            None
         
         Return:
             tuple: A list of daily wages for the 20-day period, counts of full-time, part-time, leave days, and total hours worked.
         """
+        full_day_wage, part_time_wage = cls.calculate_wage()
         
-        while self.total_days < self.MAX_WORKING_DAYS and self.total_hours < self.MAX_WORKING_HOURS:
-            attendance = 2 if self.total_hours == 96 else self.check_attendance()
+        while cls.total_days < cls.MAX_WORKING_DAYS and cls.total_hours < cls.MAX_WORKING_HOURS:
+            attendance = 2 if cls.total_hours == 96 else cls.check_attendance()
             
             match attendance:
                 case 1:
-                    self.daily_wages.append(full_day_wage)
-                    self.full_time_count += 1
-                    self.total_days += 1
-                    self.total_hours += self.FULL_DAY_HOUR
+                    cls.daily_wages.append(full_day_wage)
+                    cls.full_time_count += 1
+                    cls.total_days += 1
+                    cls.total_hours += cls.FULL_DAY_HOUR
                 
                 case 2:
-                    self.daily_wages.append(part_time_wage)
-                    self.part_time_count += 1
-                    self.total_days += 1
-                    self.total_hours += self.FULL_DAY_HOUR / 2
+                    cls.daily_wages.append(part_time_wage)
+                    cls.part_time_count += 1
+                    cls.total_days += 1
+                    cls.total_hours += cls.FULL_DAY_HOUR / 2
                 
                 case 0:
-                    self.daily_wages.append(0)
-                    self.leaves_count += 1
+                    cls.daily_wages.append(0)
+                    cls.leaves_count += 1
                     
-        return self.daily_wages, self.full_time_count, self.part_time_count, self.leaves_count, self.total_hours
+        return cls.daily_wages, cls.full_time_count, cls.part_time_count, cls.leaves_count, cls.total_hours
         
 def main():
-    employee_object = Employee_Wage()
-    full_day_wage, part_time_wage = employee_object.calculate_wage()
-    daily_wages, full_time_count, part_time_count, leaves_count, hours = employee_object.wage_for_month(full_day_wage, part_time_wage)
+    daily_wages, full_time_count, part_time_count, leaves_count, hours = Employee_Wage.wage_for_month()
     
     print(f"Per day wages of employee: {daily_wages}")
     print(f"Total Wage of the month: ${sum(daily_wages)}")
